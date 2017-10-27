52b2acbfebb5475e8f2f34ea41a2964add132195
cahyadibayu <33141007+cahyadibayu@users.noreply.github.com>
2017-10-27 13:29:20 +0700

    Add files via upload

---
+++ Source/Code.ino
@@ -0,0 +1,32 @@
+#include <Keyboard.h>
+void setup() {
+  // put your setup code here, to run once:
+pinMode(8,INPUT);
+pinMode(9,INPUT);
+pinMode(7,OUTPUT);
+Keyboard.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(digitalRead(8)==LOW)
+{
+  Keyboard.press('z');
+  digitalWrite(7,HIGH);
+}
+else
+{
+  Keyboard.release('z');
+  digitalWrite(7,LOW);
+}
+if(digitalRead(9)==LOW)
+{
+  Keyboard.press('x');
+  digitalWrite(7,HIGH);
+}
+else
+{
+  Keyboard.release('x');
+  digitalWrite(7,LOW);
+}
+}
