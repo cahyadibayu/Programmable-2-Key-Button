20f307c9478541521600f70bd9e70c6335d9b06c
cahyadibayu <33141007+cahyadibayu@users.noreply.github.com>
2017-10-27 13:35:08 +0700

    Update Code.ino

--- Source/Code.ino
+++ Source/Code.ino
@@ -1,32 +1,37 @@
 #include <Keyboard.h>
+int But1 = 16; // the pin can be everywhere
+int But2 = 14;
+int LED = 9;
+int LED2 = 10;
+
 void setup() {
-  // put your setup code here, to run once:
-pinMode(8,INPUT);
-pinMode(9,INPUT);
-pinMode(7,OUTPUT);
-Keyboard.begin();
+  
+pinMode(But1,INPUT);
+pinMode(But2,INPUT);
+pinMode(LED,OUTPUT);
+pinMode(LED2,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-if(digitalRead(8)==LOW)
+  
+if (digitalRead(But1) == HIGH )
 {
-  Keyboard.press('z');
-  digitalWrite(7,HIGH);
+  Keyboard.press('z');  // the key is for you to choose
+  digitalWrite(LED,HIGH);
 }
 else
 {
   Keyboard.release('z');
-  digitalWrite(7,LOW);
+  digitalWrite(LED,LOW);
 }
-if(digitalRead(9)==LOW)
+if (digitalRead(But2) == HIGH )
 {
-  Keyboard.press('x');
-  digitalWrite(7,HIGH);
+  Keyboard.press('x'); // the key is for you to choose
+  digitalWrite(LED2,HIGH);
 }
 else
 {
   Keyboard.release('x');
-  digitalWrite(7,LOW);
+  digitalWrite(LED2,LOW);
 }
 }
